f69ad6ee6d8d7886d7a90fb765fe434589655c97
Maurits van Dueren den Hollander <cybermaus@gmail.com>
2021-08-05 21:13:22 +0200

    iMore tweaks to personal examples

--- examples/key-mapper-exit.sh
+++ examples/key-mapper-exit.sh
@@ -7,13 +7,17 @@
 modefile='/tmp/screenblankmode'
 mode='-p'
 host=''
+macro=''
 
-# Camera devices to send CURL to should best resolve in DNS, straigt from 
+
+# Camera devices to send CURL to should best resolve in DNS, straight from 
 # their names as mentioned in KEY-MAPPER
 # User and Passwords for those devices are best stored in system .netrc file
 # where CURL will find them automatically.
 cam01='cam01'
 cam02='cam02'
+user=''
+pass=''
 
 
 # Toggle screen blanking on or off
@@ -39,15 +43,13 @@ if [ "$blank" == "screenunblank" ]; then
 	# Especially on RasPi 3B+ and 4B when in X it
 	# seems needed to jitter fb a bit to redisplay
 	fbset -move up -step 0
-	fbset -match	
 	fbset -a -move up -step 0
-	fbset -a -match	
 fi
 
 # additional useful commands related to screen blanking
 # (sometimes) works to repaint the buffer
+#	fbset -move up -step 0
 #	fbset -a -move up -step 0
-#	fbset -a -match
 # Hide and Shows blinking cursor 	
 #	sudo echo -e '\x1b[?25l' > /dev/tty1
 #	sudo echo -e '\x1b[?25h' > /dev/tty1
@@ -64,9 +66,16 @@ fi
 # if mon01 command 
 if [ "$1" == "mon01" ]; then
 
+	# Quit Camplayer command
+	if [ "$2" == "restart"  -o "$2" == "preset8" ]; then
+		macro='k(KEY_Q)'
+		#systemctl stop camplayer
+	fi
+
 	# Restart Camplayer command
 	if [ "$2" == "restart"  -o "$2" == "preset9" ]; then
-		sudo systemctl restart camplayer
+		# should not need sudo, as this runs as root
+		systemctl restart camplayer
 	fi
 fi
 
@@ -79,6 +88,7 @@ if [ "$1" == "cam01" -o "$1" == "cam02" ]; then
 	# Goto Preset command
 	action=$2
 	if [ "${action%%[0-9]*}" == "preset" ]; then
+		# reminder: user/pass are not encoded here but done by --digest -n
 		preset=${action//[!0-9]/}
 		url="http://${host}/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=${preset}&arg3=0"
 		curl --digest -n -s -g -m 1 -- "${url}"
@@ -93,7 +103,7 @@ if [ "$1" == "cam01" -o "$1" == "cam02" ]; then
 fi
 
 
-#echo $key-mapper-macro
+echo $macro
 
 exit 0
 
